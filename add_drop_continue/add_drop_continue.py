892447c0515775e994658f7b593e61c2fd6d55a8
Julian Smith De Niro <jsd410@nyu.edu>
2017-10-19 00:17:37 -0400

    Testing for course_drop complete.

--- add_drop_continue/add_drop_continue.py
+++ add_drop_continue/add_drop_continue.py
@@ -5,12 +5,18 @@ import pickle
 class AddDropContinue:
     """Class for adding, dropping, and showing user's planner."""
 
-    def add_course():
+    course_list = []
+
+    def add_course(self):
         """Add course to planner and stores in pickle file."""
         course_object = {}
 
         title = input("What is the title of the course: ")
         course_object["title"] = title
+
+        importance = input(
+            "Is this for your major(M), minor(m), or a general elective(ge): ")
+        course_object["importance"] = importance
 
         try:
             credits = int(input("How many credits is the course: "))
@@ -28,20 +34,25 @@ class AddDropContinue:
             print(
                 "Please enter a floating point value for the hours per week you \
                 attend this course")
+        AddDropContinue.course_list.append(course_object)
 
         # Stores course_object as a binary file to a pickle file
-        pickle.dump(course_object, open("user_data.p", "wb"))
+        pickle.dump(AddDropContinue.course_list, open(
+            "user_data.p", "wb"))
 
 
 def main():
     """Main method, runs initial input."""
+    user = AddDropContinue()
     flag = False
     print("\n Welcome to your planner! What would you like to do? \n")
     while not flag:
         add_drop_continue = input(
             "You can `add`(a), `drop`(d) , or `continue`(c) to your planner: ")
         if add_drop_continue == 'add' or add_drop_continue == 'a':
-            AddDropContinue.add_course()
+            user.add_course()
+        elif add_drop_continue == 'end' or add_drop_continue == 'e':
+            flag = True
         else:
             raise ValueError("Please input 'add', 'drop', or 'continue' ")
 main()
---
+++ add_drop_continue/test_add_drop_continue.py
@@ -0,0 +1,33 @@
+"""Testing framework for dropping a class from `user_data.p`."""
+import unittest
+import pickle
+
+
+class TestAddDropContinue(unittest.TestCase):
+    """A class which allows for testing of methods."""
+
+    def test_drop(self):
+        """Testing whether if the correct course_object is removed."""
+        course_list = pickle.load(open("user_data.p", "rb"))
+        print("Current course list:\n")
+
+        for course_object in course_list:
+            print(course_object["title"])
+        print("\n")
+
+        course_to_drop = input("Which course would you like to drop: ")
+        for course_object in course_list:
+            if course_object["title"] == course_to_drop:
+                course_list.remove(course_object)
+
+        pickle.dump(course_list, open("user_data.p", "wb"))
+
+        expected_result = [
+            {
+                'title': 'intro to comp sci', 'importance': 'M', 'credits': 4,
+                'hours_per_week': 2.5
+            }
+        ]
+        self.assertEqual(course_list, expected_result)
+if __name__ == '__main__':
+    unittest.main()
