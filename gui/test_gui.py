bbc55d240eb6ba86a09564f450efc2ff9f8abfeb
Julian Smith De Niro <jsd410@nyu.edu>
2017-10-19 16:25:45 -0400

    Began app testing

--- gui/test_gui.py
+++ gui/test_gui.py
@@ -1,8 +1,15 @@
 from kivy.app import App
 from kivy.uix.button import Button
+import kivy
+kivy.require('1.10.0')
+
+
 
 class TestApp(App):
     def build(self):
-        return Button(text='Hello World')
+        return Button(text='Welcome to your Planner! Click to continue')
 
 TestApp().run()
+
+if __name__ == '__main__':
+    TestApp().run()
